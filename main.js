04b98f21a4e6f24c67df2200d5a0695caffff7e7
Alisa Khaustova <alice.khaustova@gmail.com>
2020-02-12 16:08:26 +0200

    add map

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ const lat = document.querySelector('#lat');
 const time = document.querySelector('#time')
 const date = document.querySelector('#date')
 
+const countIss = document.querySelector('#countIss')
+
 let timer = null;
 
 //получение данных локации 
@@ -23,23 +25,48 @@ async function getLocation(){
 time.innerText = moment.utc().format('HH:mm:ss');
 date.innerText = moment().add(7,'days').format('dddd, Do MMMM YYYY') ;
 
-
-
 //состав текущего экипажа 
 async function getAstronaut() {
 
   try {
-    const people = await axios.get('http://api.open-notify.org/astros.json')
-    const res =  people.data;
+    const response = await axios.get('http://api.open-notify.org/astros.json')
+    const res =  response.data;
+
+     //подсчет человек в экипаже
+    let issPeople = res.people.filter(function(craft) {
+      return craft.iss === 'ISS';
+    });
+
+    //вывод карточки астронавта
     res.people.forEach(function(elem){
       let astronaut = createCard(elem);
       astronautList.appendChild(astronaut);
-     });
+     });     
 
+    //вывод количества человек 
+     countIss.innerText = issPeople.length;
+    
   }catch(error){
     console.error(error);
   }
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+  var map = new L.Map('map', {
+      center: new L.LatLng(30.455002, 30.511284),
+      zoom: 2,
+      layers: [
+          new L.TileLayer('https://tms{s}.visicom.ua/2.0.0/planet3/base/{z}/{x}/{y}.png?key=', {
+              attribution: 'Данные карт © АО «<a href="https://api.visicom.ua/">Визиком</a>»',
+              maxZoom: 19,
+              subdomains: '123',
+              tms: true
+          })
+      ]
+  });
+});
+
+
 
 getLocation();
 getAstronaut();
@@ -55,8 +82,7 @@ function createCard(res) {
   astronaut.innerHTML = `
     <div class="card bg-light ">
       <div class="card-body text-center astronaut">
-      <div>
-        <i class="fas fa-user-circle icon"></i>
+      <div class='container'>
         <i class="fas fa-user-astronaut icon"></i>
         <p class="card-text font-weight-bold">${res.name}</p>
       </div>
